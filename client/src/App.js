a2009e5a1ca6d93ae6cc62450ddb35bb2d109b8e
karafaze <fazli.carrat@gmail.com>
2023-01-18 15:15:09 +0100

    added basic components structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+
 
 export default function App() {
-    return <h1>TodoList app</h1>;
+    return (
+        <React.Fragment>
+            <Header />
+            <main></main>
+            {/* <Footer /> */}
+        </React.Fragment>
+    );
 }
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <h2 className="navbar--title">TodoList App</h2>
+            </nav>
+        </header>
+    );
+}
